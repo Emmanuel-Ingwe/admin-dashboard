9ffa47f7ae05a6dbfbba47400b5a03c7effa699e
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-02-01 01:34:24 +0100

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
+// import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+// import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './App.css';
+
 
 const App = () => {
     return (
-        <h1 className="underline text-3xl"><i class="fas fa-apper    "></i></h1>
+        <div className="">
+            <BrowserRouter>
+                <div className="flex relative dark:bg-main-dark-bg">
+                    <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+                        <TooltipComponent
+                            content="Settings"
+                            position="Top"
+                        >
+                            <button
+                                type="button"
+                                // onClick={() => setThemeSettings(true)}
+                                // style={{ background: currentColor, borderRadius: '50%' }}
+                                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+                            >
+                                <FiSettings />
+                            </button>
+                        </TooltipComponent>
+                    </div>
+                    {activeMenu ? (
+                        <div className="w-73 fixed sidebar dark:bg-secondary-dark-bg bg-white">Sidebar</div>
+                    ) : (
+                        <div>
+                            sidebar w-0
+                        </div>
+                    )}
+                </div>
+            </BrowserRouter>
+        </div>
     );
 };
 
