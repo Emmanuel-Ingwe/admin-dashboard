879fc0f591f05b4691b1556b325bd32e067cd3fe
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-02-03 05:55:42 +0100

    Pages

---
+++ src/pages/Charts/Financial.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Financial = () => {
+  return (
+    <div>YY
+    </div>
+  )
+}
+
+export default Financial
---
+++ src/pages/Charts/Line.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Line = () => {
+  return (
+    <div>
+      tt
+    </div>
+  )
+}
+
+export default Line
