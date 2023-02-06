aeaa77ae7a6a466c2ce9baf283a0a7ff50498162
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-02-06 07:40:03 +0100

    e com

--- src/App.js
+++ src/App.js
@@ -79,11 +79,11 @@ const App = () => {
                             activeMenu
                                 ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
                                 : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
-                        }
-                    >
+                        }>
                         <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
                             <Navbar />
                         </div>
+
                         <div>
                             {/* {themeSettings && (<ThemeSettings />)} */}
 
@@ -115,7 +115,7 @@ const App = () => {
 
                             </Routes>
                         </div>
-                        <Footer />
+                        {/* <Footer /> */}
                     </div>
                 </div>
             </BrowserRouter>
