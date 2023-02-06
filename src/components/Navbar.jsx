04e7547d27eaa096728856fab64d37cf64d371dd
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-02-06 07:08:01 +0100

    __

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,6 +31,26 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 const Navbar = () => {
   const { currentColor, activeMenu, setActiveMenu, handleClick, isClicked, setScreenSize, screenSize } = useStateContext();
 
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth)
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+   useEffect(() => {
+    if (screenSize <= 900) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
+  // const handleActiveMenu = () => setActiveMenu(!activeMenu);
+
 return (
   <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
     <NavButton title="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color='blue' icon={<AiOutlineMenu />} />
