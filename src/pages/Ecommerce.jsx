e05a046ce949bdb14bed78fc8acc28016240b801
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-02-08 13:06:40 +0100

    sync charts

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -78,7 +78,7 @@ const Ecommerce = () => {
           <div className="mt-10 flex gap flex-wrap justify-center">
             <div className="border-r-1 border-color m-4 pr-10"></div>
             <p>
-              <span className="text font-semibold">$93,438</span>
+              <span className="text-3xl font-semibold">$93,438</span>
               <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">23%</span>
             </p>
             <p className="text-gray-500 mt-1">Budget</p>
@@ -90,7 +90,7 @@ const Ecommerce = () => {
             <p className="text-gray-500 mt-1">Expense</p>
           </div>
 
-          {/* <div className="mt-5">
+          <div className="mt-5">
             <SparkLine  
               currentColor="blue"
               id="line-sparkline"
@@ -100,7 +100,7 @@ const Ecommerce = () => {
               data={SparklineAreaData}
               color="blue"
             />
-          </div> */}
+          </div>
 
         </div>
       </div>
