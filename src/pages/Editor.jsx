75de2cf5043efcac7c4ed5ba7b65a40e6a47753e
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-02-10 07:54:39 +0100

    head

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
 
-import { Header } from '../components';
 import { EditorData } from '../data/dummy';
+import Header from '../components/Header';
 
 const Editor = () => (
   <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
@@ -13,4 +13,6 @@ const Editor = () => (
     </RichTextEditorComponent>
   </div>
 );
+
+
 export default Editor;
