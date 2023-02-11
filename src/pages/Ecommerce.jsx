613150bb531361220046aa395e9dbd47f851d7ef
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-02-11 19:42:57 +0100

    __

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -22,7 +22,7 @@ const Ecommerce = () => {
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
-              <p className="text-2xl">$63,448.78</p>
+              <p className="text-2xl text-black">$63,448.78</p>
             </div>
             {/* <button
               type="button"
@@ -65,7 +65,7 @@ const Ecommerce = () => {
       <div className="flex gab-10 flex-wrap justify-center">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 rounded-2xl md:w-780">
           <div className="flex justify-between">
-            <p className="font-semibold text-xl">Revenue Upadates</p>
+            <p className="font-semibold text-xl text-black">Revenue Upadates</p>
             <div className="flex items-center gap-4">
               <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
                 <span><GoPrimitiveDot/></span>
@@ -80,14 +80,14 @@ const Ecommerce = () => {
           <div className="mt-10 flex gap flex-wrap justify-center">
             <div className="border-r-1 border-color m-4 pr-10"></div>
             <p>
-              <span className="text-3xl font-semibold">$93,438</span>
+              <span className="text-3xl font-semibold text-black">$93,438</span>
               <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">23%</span>
             </p>
             <p className="text-gray-500 mt-1">Budget</p>
           </div>
           <div className="mt-8">
           <p>
-              <span className="text-3xl font-semibold">$48,438</span>
+              <span className="text-3xl font-semibold text-black">$48,438</span>
             </p>
             <p className="text-gray-500 mt-1">Expense</p>
           </div>
