51502baa42709fe6c0f2d055704dfdb948a16a02
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-02-11 19:27:02 +0100

    Done w repo

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import { ChartsHeader, LineChart } from '../../components';
+import LineChart from "../../components/Charts/LineChart";
 
 const Line = () => (
   <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-    <h1 className="font-xl">Line</h1>
+    <h1 className="text-xl font-bold">Inflation Rate</h1>
     <div className="w-full">
       <LineChart />
     </div>
